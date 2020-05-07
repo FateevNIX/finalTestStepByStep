036af913e69690ba33d43e8d724921e2ab742f49
FateevNIX <aleksandr.fateev@nixsolutions.com>
2020-05-07 15:07:26 +0300

    removed unnecessary methods of openHomePagfe and openSearcResultsPage

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -1,36 +1,25 @@
 package steps;
 
 import io.qameta.atlas.core.Atlas;
+import io.qameta.atlas.webdriver.WebDriverConfiguration;
 import io.qameta.atlas.webdriver.WebPage;
+
 import org.openqa.selenium.WebDriver;
-import io.qameta.allure.Step;
+import tests.BaseTest;
 
 
-public class BaseSteps {
+public class BaseSteps extends BaseTest {
 
     public WebDriver driver;
 
-    public BaseSteps(WebDriver driver) {
-        this.driver = driver;
-    }
+    public BaseSteps(WebDriver driver) { this.driver = driver;}
+    Atlas atlas = new Atlas(new WebDriverConfiguration(driver));
+
 
     public <T extends WebPage> T on(Class<T> page) {
-        Atlas atlas = new Atlas();
+
         return atlas.create(driver, page);
     }
 
-    @Step
-    public HomePageSteps openHomePage() {
-        HomePageSteps homePageSteps = new HomePageSteps(driver);
-        driver.get(homePageSteps.getHomePageURL());
-       
-        return homePageSteps;
-    }
-    @Step
-    public SearchResultsPageSteps openSearchResultsPage() {
-        SearchResultsPageSteps searchResultsPageSteps = new SearchResultsPageSteps(driver);
-        driver.get(searchResultsPageSteps.getSearchResultsPageURL());
-        return searchResultsPageSteps;
-    }
 
 }
