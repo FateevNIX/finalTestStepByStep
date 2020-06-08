c5ea3352f1331ec445b729bbff519b851a4beb27
FateevNIX <aleksandr.fateev@nixsolutions.com>
2020-06-08 14:15:35 +0300

    Added creation of product List and page objects

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,18 +5,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import steps.HomePageSteps;
+import steps.ProductDetailsPageSteps;
+import testData.Product;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class BaseTest {
     public String baseURL = ("http://automationpractice.com");
     public WebDriver driver;
-
+    List<Product> products = new ArrayList<>();
+    HomePageSteps onHomePageSteps;
+    ProductDetailsPageSteps onProductDetailsPageSteps;
 
     @BeforeClass
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get(baseURL);
+        onHomePageSteps = new HomePageSteps(driver);
+        onProductDetailsPageSteps = new ProductDetailsPageSteps(driver);
     }
 
     @AfterClass
