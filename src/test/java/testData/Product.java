8b9bdf613bf08bc20dbc18659b8bb68b4daf90c5
FateevNIX <aleksandr.fateev@nixsolutions.com>
2020-06-08 14:21:01 +0300

    Added Product class for more convenient data generation

---
+++ src/test/java/testData/Product.java
@@ -0,0 +1,16 @@
+package testData;
+
+public class Product {
+    public String name;
+    public String size;
+    public int quantity;
+    public double price;
+
+    public Product (String name, String size, int quantity, double price ){
+        this.name = name;
+        this.size = size;
+        this.quantity = quantity;
+        this.price = price;
+
+    }
+}
