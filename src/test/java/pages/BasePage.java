2e04b38ae850055776840dba51a65a22ee603a14
FateevNIX <aleksandr.fateev@nixsolutions.com>
2020-06-08 13:52:41 +0300

    Removed unescessary extend

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,10 +1,8 @@
 package pages;
 
-import io.qameta.atlas.webdriver.AtlasWebElement;
 import io.qameta.atlas.webdriver.WebPage;
 import layouts.WithHeader;
 
-
-public interface BasePage extends WebPage, AtlasWebElement, WithHeader {
+public interface BasePage extends WebPage, WithHeader {
 
 }
