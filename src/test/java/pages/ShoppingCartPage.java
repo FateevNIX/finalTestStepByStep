cd91987abd9904c78fecb282752d47ddd793b1af
FateevNIX <aleksandr.fateev@nixsolutions.com>
2020-06-08 14:51:12 +0300

    Added block ProductItem

--- src/test/java/pages/ShoppingCartPage.java
+++ src/test/java/pages/ShoppingCartPage.java
@@ -1,26 +1,11 @@
 package pages;
 
-import io.qameta.atlas.webdriver.ElementsCollection;
+import blocks.shoppingCartPageBlocks.ProductItem;
 import io.qameta.atlas.webdriver.extension.FindBy;
-import org.openqa.selenium.WebElement;
 import tests.HtmlElement;
 
 public interface ShoppingCartPage extends BasePage{
-    @FindBy("//span[contains(@id, 'total_product_price')]")
-    ElementsCollection<WebElement> totalPrice();
 
-    @FindBy("//td/p[@class='product-name']/a")
-    ElementsCollection<WebElement> productName();
-
-    @FindBy("//input[@class='cart_quantity_input form-control grey']")
-    ElementsCollection<WebElement> quantityOfProduct();
-
-    @FindBy("//td[@class='cart_description']/small/a")
-    ElementsCollection<WebElement> colourAndSize();
-
-    @FindBy("//i[@class='icon-trash']")
-    HtmlElement trashcanButton();
-
-    @FindBy("//span[@id='summary_products_quantity']")
-    HtmlElement shoppingCartContains();
+    @FindBy(".//div[@id='order-detail-content']")
+    ProductItem products();
 }
