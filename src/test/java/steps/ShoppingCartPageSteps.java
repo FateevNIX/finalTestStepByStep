8eb146b668561d33b8538eba6e52f7c3160933da
FateevNIX <aleksandr.fateev@nixsolutions.com>
2020-06-08 15:00:22 +0300

    Refactored all methods

--- src/test/java/steps/ShoppingCartPageSteps.java
+++ src/test/java/steps/ShoppingCartPageSteps.java
@@ -1,17 +1,14 @@
 package steps;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import pages.ShoppingCartPage;
+import testData.Product;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import static matchers.HasTextMatcher.hasText;
-import static matchers.IsElementDisplayedMatcher.isDisplayed;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.*;
 
 public class ShoppingCartPageSteps extends BaseSteps {
 
@@ -20,68 +17,49 @@ public class ShoppingCartPageSteps extends BaseSteps {
     }
 
     public List<String> getProductNames() {
-        List<WebElement> resultList = onShoppingCartPage().productName();
-        List<String> titlesOfProducts = new ArrayList<String>();
-        for (WebElement resultItem : resultList) {
-            titlesOfProducts.add(resultItem.getText());
-        }
-        return titlesOfProducts;
+        return onShoppingCartPage().products().description()
+                .extract(description -> description.productName().getText());
     }
 
     public List<Integer> getProductQuantity() {
-        List<WebElement> resultList = onShoppingCartPage().quantityOfProduct();
-        List<Integer> quantityOfProducts = new ArrayList<Integer>();
-        for (WebElement resultItem : resultList) {
-            quantityOfProducts.add(Integer.parseInt(resultItem.getAttribute("value")));
-        }
-        return quantityOfProducts;
+        return onShoppingCartPage().products().quantityOfProduct()
+                .extract(quantity -> Integer.parseInt(quantity.getAttribute("value")));
     }
 
     public List<Double> getProductPrice() {
-        List<WebElement> resultList = onShoppingCartPage().totalPrice();
-        List<Double> productPrices = new ArrayList<Double>();
-        for (WebElement resultItem : resultList) {
-            productPrices.add(Double.parseDouble(resultItem.getText().replace("$", "")));
-        }
-        return productPrices;
+        return onShoppingCartPage().products().price()
+                .extract(prices -> Double.parseDouble(prices.getText().replace("$", "")));
     }
 
     public List<String> getProductSize() {
-        List<WebElement> resultList = onShoppingCartPage().colourAndSize();
-        List<String> sizeOfProduct = new ArrayList<String>(resultList.size());
-        for (WebElement resultItem : resultList) {
-            int lastChar = resultItem.getText().length() - 1;
-            sizeOfProduct.add(resultItem.getText().substring(lastChar));
-        }
-        return sizeOfProduct;
+        return onShoppingCartPage().products().description()
+                .extract(description -> description.colourAndSize().getText()
+                        .substring(description.colourAndSize().getText().length() - 1));
     }
 
-    public ShoppingCartPageSteps checkAllDataMatchPreviouslySelectedValues(List<String> productName, String[] size, int[] quantity, List<Double> productPrice) {
-        int i = 0;
-
-        while (i < getProductNames().size()) {
-            assertThat(getProductNames().get(i).toLowerCase(), equalTo(productName.get(i).toLowerCase()));
-            assertThat(getProductSize().get(i), equalTo(size[i]));
-            assertThat(getProductQuantity().get(i), equalTo(quantity[i]));
-            assertThat(getProductPrice().get(i), equalTo(productPrice.get(i)));
-            i++;
+    public ShoppingCartPageSteps checkAllDataMatchPreviouslySelectedValues(List<Product> products) {
+        for (int i = 0; i < getProductNames().size(); i++) {
+            assertThat(getProductNames().get(i).toLowerCase(), stringContainsInOrder(Arrays.asList(products.get(i).name.toLowerCase().split(" "))));
+            assertThat(getProductSize().get(i), equalTo(products.get(i).size));
+            assertThat(getProductQuantity().get(i), equalTo(products.get(i).quantity));
+            assertThat(getProductPrice().get(i), equalTo(products.get(i).price));
         }
         return this;
     }
 
     public ShoppingCartPageSteps deleteFirstProductFromCart() {
-        onShoppingCartPage().trashcanButton().should(isDisplayed()).click();
+        click(onShoppingCartPage().products().deleteIcon().get(0));
         return this;
     }
 
-    public ShoppingCartPageSteps checkFirstProductWasDeleted(List<String> productsName) {
-        onShoppingCartPage().shoppingCartContains().should(hasText("1 Product")); //это костыльно, но пока лучше придумать не смог
-        assertThat(getProductNames().get(0).toLowerCase(), not(productsName.get(0).toLowerCase()));
+    public ShoppingCartPageSteps checkFirstProductWasDeleted(List<Product> products) {
+        onShoppingCartPage().products().description().should(hasSize(1));//проверка что юайка успешно обновилось и продукт уже исчез
+        assertThat(getProductNames().get(0).toLowerCase(), not(products.get(0).name.toLowerCase())); //проверка что именно первый продукт был удален
         return this;
     }
 
-    public ShoppingCartPageSteps checkSecondProductIsInList(List<String> productsName) {
-        assertThat(getProductNames().get(0).toLowerCase(), equalTo(productsName.get(1).toLowerCase()));
+    public ShoppingCartPageSteps checkSecondProductIsInList(List<Product> products) {
+        assertThat(getProductNames().get(0).toLowerCase(), equalTo(products.get(1).name.toLowerCase()));
         return this;
     }
 
